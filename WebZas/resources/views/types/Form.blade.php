439f42565fc5e90b97e28117754ea2eee6191c64
Riu <josemanuelriu@gmail.com>
2026-03-10 10:47:08 +0100

    Added description types

--- WebZas/resources/views/types/Form.blade.php
+++ WebZas/resources/views/types/Form.blade.php
@@ -9,4 +9,12 @@
         class="w-full mt-1 border-zas-primary border rounded-lg shadow-sm focus:ring-zas-primary focus:border-zas-primary pl-2">
     @error('type') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
 </div>
+<div>
+    <label class="block font-medium text-gray-700">Descripción</label>
+    <textarea name="description"
+        class="w-full mt-1 border-zas-primary border rounded-lg shadow-sm focus:ring-zas-primary focus:border-zas-primary pl-2 resize-none">
+        {{ old('type', $type->Description ?? '') }}
+    </textarea>
+    @error('description') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
+</div>
 
