89f186d42326927b03354c5f794b25f299c5972f
Riu <josemanuelriu@gmail.com>
2026-09-04 09:19:23 +0200

    delete old variable Name

--- WebZas/resources/views/zassessions/show.blade.php
+++ WebZas/resources/views/zassessions/show.blade.php
@@ -78,7 +78,7 @@ $canCreateGame = in_array($user->type, ['admin','junta','partner']);
 
             <div class="flex gap-2 mt-10">
                 @if($isJoined && !$isFull)
-                    <form method="POST" action="{{ route('zassessions.externaljoin', [$zassession, $Name]) }}">
+                    <form method="POST" action="{{ route('zassessions.externaljoin', $zassession) }}">
                         @csrf
                         <input type="text" name="external_name" placeholder="{{ __('messages.Name') }}" required>
                         <button type="submit"
